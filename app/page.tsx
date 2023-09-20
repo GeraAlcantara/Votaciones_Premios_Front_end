9500ad95344b0d776df9e3e2d079583f8bdbf3eb
linuxmobile <bdiez19@gmail.com>
2023-09-20 13:32:09 -0300

    🏁: Starting Project w/ Next.js + TailwindCSS + TS

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-5xl font-black text-center max-w-7xl sm:text-6xl md:text-9xl">
+        Premios Discord MiduDev 2023
+      </h1>
+    </main>
+  );
+}
