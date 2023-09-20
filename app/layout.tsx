70dc6151849da7c6902f740d282164bb9ebccf05
linuxmobile <bdiez19@gmail.com>
2023-09-20 17:18:24 -0300

    🌈: added colorscheme to TailwindCSS config

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-brand-light text-brand-dark dark:bg-brand-dark dark:text-brand-light`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      theme: {
+        colors: {
+          'brand-light': '#F7F1F9',
+          'brand-dark': '#000000',
+          'brand-yellow': '#D3F527',
+          'brand-orange': '#FF930F',
+          'brand-purple': '#4A28F2',
+          'brand-lila': '#A998F8', 
+        },
+      },
     },
   },
   plugins: [],
