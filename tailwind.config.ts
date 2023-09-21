4ecda64d7802cf08afecc77f8ce935cf29c53b95
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2023-09-20 19:19:39 -0500

    adding format eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,13 @@ const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
       theme: {
         colors: {
@@ -20,11 +19,12 @@ const config: Config = {
           'brand-yellow': '#D3F527',
           'brand-orange': '#FF930F',
           'brand-purple': '#4A28F2',
-          'brand-lila': '#A998F8',
-        },
-      },
-    },
+          'brand-lila': '#A998F8'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
+
 export default config
