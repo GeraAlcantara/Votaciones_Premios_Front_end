29fc2208f4d93627e9b166f2be8427f96431984a
linuxmobile <bdiez19@gmail.com>
2023-09-27 17:17:54 -0300

    🐞: fixing colors from tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
 
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-import { ThemeProvider } from '@/lib/theme-provider'
-import { ThemeSwitcher } from '@/components/ThemeSwitcher'
+import { ThemeProvider } from "@/lib/theme-provider";
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Los Premios Discord Midudev 2023',
+  title: "Los Premios Discord Midudev 2023",
   description:
-    'Es hora de revelar tus habilidades únicas. Participa en las categorías más originales y emocionantes del servidor de Midudev. ¡La gloria te espera!'
-}
+    "Es hora de revelar tus habilidades únicas. Participa en las categorías más originales y emocionantes del servidor de Midudev. ¡La gloria te espera!",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="es">
       <body
-        className={`${inter.className} bg-brand-light text-brand-dark dark:brand-dark dark:text-brand-light`}
+        className={`${inter.className} bg-brand-light text-brand-dark dark:bg-brand-dark dark:text-brand-light`}
       >
         <ThemeProvider enableSystem attribute="class" defaultTheme="system">
           <ThemeSwitcher />
@@ -26,5 +30,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
